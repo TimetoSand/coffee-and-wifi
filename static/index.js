670baddf9cc1867b4dd2242ba243d20e327536aa
Elrond <ozturknuri8qgmail.com>
2022-06-15 02:40:44 -0700

    Update for first time

--- static/index.js
+++ static/index.js
@@ -1,6 +1,6 @@
 const button = document.querySelectorAll("#del");
 button.forEach((btn) => {
   btn.addEventListener('click', function(e){
-    btn.classList.add("pirlo")
+    btn.classList.add("pirlo");
   })
-})
+});
